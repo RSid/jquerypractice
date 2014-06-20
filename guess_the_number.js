28b222a94b4713f87a544a978e8e1f71dc0a51c4
RSid <sm1th303@gmail.com>
2014-06-20 11:24:31 -0400

    sees user's guess, compares to answer and tells user if it matched or not.

--- guess_the_number.js
+++ guess_the_number.js
@@ -7,9 +7,28 @@ $('#max').text(max);
 function getRandomNumber() {
   answer = Math.round((Math.random() * max) + 1);
 }
+getRandomNumber();
 
 
-// getRandomNumber();
+$(document).ready(function(){
+    $("#guess").keyup(function(){
+      // debugger
+        guess = $('#guess').val()
+
+        if (guess == answer) {
+            alert('Nice! You win')
+
+          } else {
+
+            alert('Nope! Guess again.')
+
+          }
+
+    });
+
+})
+
+
 // var name = prompt("What's your name?");
 // guess = Number(prompt('Hi ' + name + '! Guess a number between 1 and 10!'));
 
